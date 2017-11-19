ff9e8cdd88b823317aa7ca7af04764059e06d4e5
Jane Shkyrmetoffa <shkyrmetoffa@gmail.com>
2017-11-19 10:22:09 +0200

    readme created

--- src/app.js
+++ src/app.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import { Navigation } from './Navigation';
+import { App } from './app-component';
 
-const time = React.createElement('time', null, ` Time is ${new Date().toLocaleTimeString()}`);
-const Component = ( <
-    div >
-    <
-    h1 > Hello, react < /h1> <
-    Navigation / >
-    <
-    /div>
-);
-
-/*React.createEle ment('h1', null, 'Hello? react easycode!', time);*/
-ReactDom.render(Component, document.getElementById('app'));
+ReactDom.render(App, document.getElementById('app'));
