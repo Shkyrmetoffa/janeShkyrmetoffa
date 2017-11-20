7f4fa3b5e2285a6acf5643aa8840ded2b090e4ca
Jane Shkyrmetoffa <shkyrmetoffa@gmail.com>
2017-11-20 23:57:29 +0200

    aside added

---
+++ src/main/aside/Aside.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './aside.scss'
+import { Navigation } from '../../navigation/index';
+
+export const Aside = () => {
+    const text = 'Aside';
+    return ( 
+        <div className="aside-block">
+        <h2>{text}</h2>
+        <Navigation />
+        </div>
+    );
+}
