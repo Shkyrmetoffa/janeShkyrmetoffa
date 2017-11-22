bbac48004a321682521ffc4bea49fe43864fb84d
Jane Shkyrmetoffa <shkyrmetoffa@gmail.com>
2017-11-22 14:21:19 +0200

    Greeting component created

--- src/main/content/Content.js
+++ src/main/content/Content.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Greeting } from './greeting';
 import { Numbers } from './numbers';
 import './content.scss'
 
@@ -8,6 +9,7 @@ export const Content = () => {
     return ( 
         <div className="content-block">
         <h2>{text}</h2>
+        <Greeting name="Vasya" />
           <Numbers 
             from='2' 
             to='6' 
