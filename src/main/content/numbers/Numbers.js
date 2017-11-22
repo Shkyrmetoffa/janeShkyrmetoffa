8f0356d6d965af34e4603ae05755d622c492418d
Jane Shkyrmetoffa <shkyrmetoffa@gmail.com>
2017-11-22 16:00:44 +0200

    Numbers created

--- src/main/content/numbers/Numbers.js
+++ src/main/content/numbers/Numbers.js
@@ -3,7 +3,28 @@ import React from 'react';
 import './numbers.scss'
 
 export const Numbers = (props) => {
+    let arr = [];
+    for(let i = +props.from; i <= props.to; i++) {
+        arr.push(i);
+    }
+    
+    if(props.odd !== undefined) {
+        arr = arr.filter(num => num % 2);
+    }
+    if(props.even !== undefined) {
+        arr = arr.filter(num => {
+            if(num % 2 == 0) return num;
+        });
+    }
     return ( 
-        <div className = "num-list"></div>
+        <div className = "num-list">
+            <ul>
+                { 
+                    arr.map((num, index) => 
+                       <li key={ index }>{ num }</li>
+                    )
+                }
+            </ul>
+        </div>
     );
 }
