19ec6e6a3e76eb792dd7345cd75db59664268a0d
Jane Shkyrmetoffa <shkyrmetoffa@gmail.com>
2017-11-28 14:59:14 +0200

    fixed

--- src/main/content/numbers/Numbers.js
+++ src/main/content/numbers/Numbers.js
@@ -4,14 +4,14 @@ import './numbers.scss'
 
 export const Numbers = (props) => {
     let arr = [];
-    for(let i = +props.from; i <= props.to; i++) {
+    for (let i = +props.from; i <= props.to; i++) {
         arr.push(i);
     }
     
-    if(props.odd !== undefined) {
+    if (props.odd !== undefined) {
         arr = arr.filter(num => num % 2);
     }
-    if(props.even !== undefined) {
+    if (props.even !== undefined) {
         arr = arr.filter(num => {
             if(num % 2 == 0) return num;
         });
