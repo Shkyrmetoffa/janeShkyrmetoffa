3cdf34008788df44cccfc0b012a79ff9b031491a
Jane Shkyrmetoffa <shkyrmetoffa@gmail.com>
2017-12-07 21:11:25 +0200

    test

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,12 +1,7 @@
 import './navigation.scss';
 
-const nav = [
-  { id: 1, name: 'Home' },
-  { id: 4, name: 'Dashboard' },
-  { id: 6, name: 'Next' }
-];
-export const Navigation = ({ title, numered }) => {
-  const items = nav
+export const Navigation = ({ list, title, numered }) => {
+  const items = list
     .map(item => <li key={item.id}>{item[title]}</li>);
   return numered
     ? <ol>{items}</ol>
--- src/partials/header/Header.js
+++ src/partials/header/Header.js
@@ -1,11 +1,15 @@
 import './header.scss';
 import { Navigation } from '../../components/navigation';
 
-
+const nav = [
+  { id: 1, name: 'Home' },
+  { id: 4, name: 'Dashboard' },
+  { id: 6, name: 'Next' }
+];
 export class Header extends React.Component {
   render() {
     return <header className="header" >
-    <Navigation title="name" />
+    <Navigation list={nav} title="name" />
   </header>
   }
 }
