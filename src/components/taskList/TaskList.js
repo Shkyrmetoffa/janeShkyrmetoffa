579b638d140286e6835d226fe0398858a1659995
Jane Shkyrmetoffa <shkyrmetoffa@gmail.com>
2017-12-11 15:30:01 +0200

    taskLisk  added

---
+++ src/components/taskList/TaskList.js
@@ -0,0 +1,29 @@
+import { Tabs, TabLink, Tab, TabContent } from 'components/tabs';
+
+import './taskList.scss';
+
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thir', 'Fri', 'Sat'];
+const getDay = () => days[new Date().getDay()];
+const dayIndex = () => new Date().getDay;
+
+export const TaskList = () => (
+  <div className="tabs-block">
+    <Tabs
+      selectedIndex={dayIndex}
+    >
+      {
+      days.map(day => (
+        <Tab>
+          <TabLink title={day} />
+          <TabContent>
+            <h3>Tab header 1</h3>
+            <p>Some content is here</p>
+            <time>{getDay()}</time>
+            <button>Add new</button>
+          </TabContent>
+        </Tab>
+      ))
+  }
+    </Tabs>
+  </div>
+);
